7ea08aa8dbfffad7e3c66b10d5f46b620d7b6df8
Yogesh188 <yogesh.kumbhar188@gmail.com>
2023-01-05 19:40:25 +0530

    JS working (console.log="clicked") with button click

---
+++ app.js
@@ -0,0 +1,9 @@
+var btnTranslate = document.querySelector("#btn");
+
+// console.log("app.js working.");
+
+function clickHandler() {
+    console.log("clicked!");
+};
+
+btnTranslate.addEventListener("click",clickHandler);
